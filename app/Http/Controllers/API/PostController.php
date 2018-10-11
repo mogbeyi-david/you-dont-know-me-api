87cf682172c0a1f2cce628c9f9d9896f1819d571
DavidMogbeyi <davidm@l5lab.com>
2018-10-11 17:11:51 +0100

    Implemented image upload

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\API;
 use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class PostController extends Controller
 {
@@ -26,9 +27,13 @@ class PostController extends Controller
         if ($input['post_type_id'] == '1') {
             return $this->storeText($input);
         }
+
+        if ($input['post_type_id'] == '2') {
+            return $this->storeImage($request);
+        }
     }
 
-    public function storeText($data)
+    protected function storeText($data)
     {
         $createTextPost = Post::create($data);
         if ($createTextPost) {
@@ -37,4 +42,27 @@ class PostController extends Controller
             return response()->json(['status' => 'error', 'data' => "Service temporarily unavailable"], 503);
         }
     }
+
+    protected function storeImage(Request $request)
+    {
+        $this->validate($request, [
+            'post' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($request->hasFile('post')) {
+            $image = $request->file('post');
+            $name = time() . '.' . $image->getClientOriginalExtension();
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $name);
+            Post::create([
+                'post_type_id' => $request['post_type_id'],
+                'post' => $name,
+                'caption' => $request['caption']
+            ]);
+            return response(['status' => 'success', 'data' => null], 201);
+        } else {
+            return response(['status' => 'error', 'data' => 'File could not be uploaded at this time'], 503);
+        }
+    }
+
 }
