35efbfa259f20d26bbcd931e191463aa5cf601fe
DavidMogbeyi <davidm@l5lab.com>
2018-10-16 16:02:48 +0100

    Returned appropriate API response

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
@@ -10,6 +11,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return Post::with('comments')->get();
+        try {
+            $data = Post::with('comments')->get();
+        } catch (QueryException $exception) {
+            return reponse()->json([
+                "status" => "error",
+                "message" => "Posts could not be fetched at this time",
+                "data" => null
+            ], 503);
+        }
+        return response()->json([
+            "status" => "success",
+            "data" => $data
+        ]);
     }
 }
